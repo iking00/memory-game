b9f4b727b9a77f8e322fa2d5bc2d4194ba000ab3
Ian King <ian_king@comcast.net>
2017-11-04 22:25:53 -0400

    feat: Build html for cards
    Set global array of icons. Shuffle array and append li with icons to ul.gameboard.

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const CARDS = [
+	'fa-anchor',
+	'fa-anchor',
+	'fa-car',
+	'fa-car',
+	'fa-bell',
+	'fa-bell',
+	'fa-bug',
+	'fa-bug',
+	'fa-diamond',
+	'fa-diamond',
+	'fa-bomb',
+	'fa-bomb',
+	'fa-paw',
+	'fa-paw',
+	'fa-tree',
+	'fa-tree',
+];
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/**
+* @description Traverses array and appends li with icons 
+*/
+function initGameBoard(){
+	const cards = shuffle(CARDS);
+	for (const card of cards) {
+		const cardHtml = '<li class="game-card"><i class="fa ' + card + '" aria-hidden="true"></i></li>';
+		$('ul.game-board').append(cardHtml);
+	}
+}
+
+$(function(){
+	initGameBoard();
+});
